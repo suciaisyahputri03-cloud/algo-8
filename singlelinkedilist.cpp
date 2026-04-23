0c55a4e9286b345d8b54a7bc366233df9a904051
suciaisyahputri03-cloud <suciaisyahputri03@gmail.com>
2026-04-23 12:31:50 +0700

    menambahkan fungsi addnode dengan validasi nomhs tdk boleh duplikat

--- singlelinkedilist.cpp
+++ singlelinkedilist.cpp
@@ -41,5 +41,22 @@ public:
             return;
         }
 
+        node *previous = START;
+        node *current = START;
 
-}
+        while (current != NULL && nim > current->noMhs)
+        {
+            if (nim == current->noMhs)
+            {
+                cout << "\nDuplikasi noMhs tidak diijinkan\n";
+                return;
+            }
+            previous = current;
+            current = current->next;
+        }
+
+        nodeBaru->next = current;
+        previous->next = nodeBaru;
+    }
+
+    
